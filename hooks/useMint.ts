2280d3f0ab9bdfe3a6169bd575a4c41722e8df9b
Salief <salieflewis@gmail.com>
2023-04-20 21:04:19 -0400

    add typings

--- hooks/useMint.ts
+++ hooks/useMint.ts
@@ -1,67 +1,57 @@
-// @ts-nocheck
+import { zoraCreator1155Impl_abi } from 'contracts/zoraCreator1155Impl_abi'
+import {
+  usePrepareContractWrite,
+  useContractWrite,
+  useWaitForTransaction,
+  useContractEvent,
+} from 'wagmi'
+import { utils, BigNumber } from 'ethers'
 
-import { zoraCreator1155Impl_abi } from "contracts/zoraCreator1155Impl_abi";
-import { useAccount, usePrepareContractWrite, useContractWrite, useWaitForTransaction, useContractEvent } from "wagmi";
-import { utils } from "ethers"
+const zora1155ToMint = '0xCfc1B8B784938A158c2729890E0348d558Af843c'
+const zoraFixedPriceStrategyGoerli = '0x10f7d171fbf4ac14a3250e9313D97b2Dfe30EcfD'
+const zoraGoerliFee = 10000
 
-const zora1155ToMint = "0xCfc1B8B784938A158c2729890E0348d558Af843c"
-const zoraFixedPriceStrategyGoerli = "0x10f7d171fbf4ac14a3250e9313D97b2Dfe30EcfD"
-const zoraGoerliFee = "10000"
-const zoraMainnetFee = "777000000000000"
-
+// const zoraMainnetFee = '777000000000000'
 
 export function useMint() {
+  const { config: prepareConfig, error: prepareError } = usePrepareContractWrite({
+    address: zora1155ToMint, // address of collection you are minting
+    abi: zoraCreator1155Impl_abi,
+    functionName: 'mint',
+    args: [
+      zoraFixedPriceStrategyGoerli, // minter set on contract
+      BigNumber.from(1), // tokenId hardcoded as 1
+      BigNumber.from(1), // quantity hardcoded as 1
+      utils.defaultAbiCoder.encode(
+        ['address'],
+        ['0x153D2A196dc8f1F6b9Aa87241864B3e4d4FEc170']
+      ) as `0x${string}`,
+    ],
+    overrides: { value: zoraGoerliFee }, // fee hardcoded as goerli fee x 1
+    // chainId: 5,
+  })
 
-    const { address } = useAccount()
+  console.log('Contract prepare error', prepareError)
 
+  const {
+    write,
+    data,
+    error: writeError,
+    isError,
+    isLoading,
+    isSuccess,
+    status,
+  } = useContractWrite(prepareConfig)
 
-    const addressMinting = address ? address : ""
-
-    console.log("address minting", addressMinting)
-
-    const readyToMint = !!address ? true : false;
-
-    console.log("ready to mint:", readyToMint)
-
-    const {config, error} = usePrepareContractWrite({
-        address: zora1155ToMint, // address of collection you are minting
-        abi: zoraCreator1155Impl_abi,
-        args: [
-            zoraFixedPriceStrategyGoerli, // minter set on contract
-            1, // tokenId hardcoded as 1
-            1, // quantity hardcoeded as 1
-            utils.defaultAbiCoder.encode(
-                ["address"],
-                ["0x153D2A196dc8f1F6b9Aa87241864B3e4d4FEc170"]
-            )
-        ],
-        enabled: readyToMint,
-        overrides: {value: 10000} // fee hardcoded as goerli fee x 1
-    })
-
-    console.log("error", error)
-
-    const { 
-        write,
-        data,
-        error: writeError,
-        isError,
-        isLoading,
-        isSuccess,
-        status
-    } = useContractWrite(config)     
-    
-    
-
-    return {
-        write,
-        data,
-        error: writeError,
-        isError,
-        isLoading,
-        isSuccess,
-        status
-    }
+  return {
+    write,
+    data,
+    error: writeError,
+    isError,
+    isLoading,
+    isSuccess,
+    status,
+  }
 }
 
 export default useMint
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,7 @@
-// @ts-nocheck
-
 import type { NextPage } from 'next'
 import { useMint } from '../hooks/useMint'
 
 const Home: NextPage = () => {
-
   const {
     write,
     data,
@@ -12,17 +9,16 @@ const Home: NextPage = () => {
     isError,
     isLoading,
     isSuccess,
-    status
+    status,
   } = useMint()
 
   return (
     <>
       <section id="home-page" className="pb-6 flex flex-row justify-center h-screen">
         <div className="w-full flex flex-row justify-center items-center">
-          <button 
-            onClick={()=>write()}
-            className="border-[1px] border-black px-4 py-2 hover:bg-black hover:text-white"
-          >
+          <button
+            onClick={() => write?.()}
+            className="border-[1px] border-black px-4 py-2 hover:bg-black hover:text-white">
             Mint
           </button>
         </div>
